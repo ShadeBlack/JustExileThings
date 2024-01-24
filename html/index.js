d1b8c617d51ecfda0d8ea23e5b7c860233342b6f
ShadeBlack <kyle@dbitsystemsltd.co.uk>
2024-01-24 21:14:27 +0000

    Badges

--- html/index.js
+++ html/index.js
@@ -11,6 +11,19 @@ const links = [
 	},
 ];
 
+const badges = [
+	{
+		label: "Github Repo",
+		src: "github-mark-white.svg",
+		url: "https://github.com/ShadeBlack/JustExileThings",
+	},
+	{
+		label: "Buy Me A Coffee",
+		src: "buymeacoffee.png",
+		url: "https://buymeacoffee.com/ShadeBlack",
+	},
+];
+
 // when ready
 document.addEventListener("DOMContentLoaded", () => {
 	let l = document.getElementById("links");
@@ -38,5 +51,24 @@ document.addEventListener("DOMContentLoaded", () => {
 		a.innerHTML = en;
 		a.href = eu;
 		l.append(a);
+	});
+
+	let b = document.getElementById("badges");
+	badges.forEach((e) => {
+		let bl = e.label;
+		let bs = e.src;
+		let bu = e.url;
+		let h = `
+			<!-- badge: ${bl} -->
+			<a href="${bu}" target="_blank" class="inline-block">
+				<div class="p-2 rounded border-2 border-gray-800 hover:border-gray-700 bg-white/10 hover:bg-white/20 transition-colors">
+					<div class="flex">
+						<img src="img/${bs}" alt="${bl} Icon" class="h-6 w-6 rounded-full" />
+						<div class="pl-2 text-sm self-center">${bl}</div>
+					</div>
+				</div>
+			</a>
+		`;
+		b.innerHTML += h;
 	});
 });
